f2d0718402ad75faa7fc3b96ab66420cbe09f687
diegorpitoco <diegorpitoco@gmail.com>
2023-04-18 22:21:09 -0300

    primeiro commit

--- src/main/java/br/com/utfpr/aulaio/App.java
+++ src/main/java/br/com/utfpr/aulaio/App.java
@@ -4,10 +4,8 @@ package br.com.utfpr.aulaio;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello Word 2l!");
     }
 }
